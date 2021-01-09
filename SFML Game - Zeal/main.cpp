af519720eb79785eb35cd0384b7acaeb4fda5d5a
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2021-01-09 14:57:45 +0600

    VS Setup Final

--- SFML Game - Zeal/main.cpp
+++ SFML Game - Zeal/main.cpp
@@ -1,22 +1,24 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+ 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(1366, 768), "ZEAL!");
+    
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
         window.display();
     }
 
